edd12ecf4a8f147e3dc564d7428645076dd266ff
lemon4ex <netchen@vip.qq.com>
2018-03-15 18:45:57 +0800

    修复一些问题

---
+++ templates/Cocoa Touch Library.xctemplate/___PACKAGENAMEASIDENTIFIER___.h
@@ -0,0 +1,13 @@
+//  weibo: http://weibo.com/xiaoqing28
+//  blog:  http://www.alonemonkey.com
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
