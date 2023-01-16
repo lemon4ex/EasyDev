45d4beb1877d3ae911803c9e230a86d649f6be00
lemon4ex <netchen@vip.qq.com>
2023-01-16 16:43:17 +0800

    更新Cocoa App Plugin 开发模板

--- templates/Cocoa App Plugin.xctemplate/Cocoa___PACKAGENAMEASIDENTIFIER___.h
+++
@@ -1,9 +0,0 @@
-//
-//  ___FILENAME___
-//  ___PACKAGENAME___
-//
-//  Created by ___FULLUSERNAME___ on ___DATE___.
-//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
